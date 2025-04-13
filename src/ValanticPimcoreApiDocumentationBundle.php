005a91ec2085e44991f8254f1b7ab8e23ec66a57
mjakopec <matej.jakopec@factory.hr>
2025-04-14 01:56:58 +0200

    remove pimcore dependency

--- src/ValanticPimcoreApiDocumentationBundle.php
+++ src/ValanticPimcoreApiDocumentationBundle.php
@@ -4,29 +4,18 @@ declare(strict_types=1);
 
 namespace Valantic\PimcoreApiDocumentationBundle;
 
-use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
-use Pimcore\Extension\Bundle\PimcoreBundleAdminClassicInterface;
-use Pimcore\Extension\Bundle\Traits\BundleAdminClassicTrait;
-use Pimcore\Extension\Bundle\Traits\PackageVersionTrait;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Valantic\PimcoreApiDocumentationBundle\DependencyInjection\CompilerPass\ApiControllerCompilerPass;
 use Valantic\PimcoreApiDocumentationBundle\DependencyInjection\CompilerPass\DataTypeParserCompilerPass;
 
-class ValanticPimcoreApiDocumentationBundle extends AbstractPimcoreBundle implements PimcoreBundleAdminClassicInterface
+class ValanticPimcoreApiDocumentationBundle extends Bundle
 {
-    use BundleAdminClassicTrait;
-    use PackageVersionTrait;
-
     public function build(ContainerBuilder $container): void
     {
         parent::build($container);
 
         $container->addCompilerPass(new ApiControllerCompilerPass());
         $container->addCompilerPass(new DataTypeParserCompilerPass());
-    }
-
-    protected function getComposerPackageName(): string
-    {
-        return 'valantic/pimcore-api-documentation';
     }
 }
