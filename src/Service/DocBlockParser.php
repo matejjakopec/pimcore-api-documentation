ca9e231e4842dc58488beaf00dfe104b720d862e
mjakopec <matej.jakopec@factory.hr>
2025-04-14 02:23:56 +0200

    remove pimcore dependency

--- src/Service/DocBlockParser.php
+++ src/Service/DocBlockParser.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Valantic\PimcoreApiDocumentationBundle\Service;
 
-use Doctrine\Common\Annotations\PhpParser;
 use PHPStan\PhpDocParser\Ast\PhpDoc\ParamTagValueNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocChildNode;
 use PHPStan\PhpDocParser\Ast\PhpDoc\VarTagValueNode;
@@ -13,24 +12,30 @@ use PHPStan\PhpDocParser\Parser\ConstExprParser;
 use PHPStan\PhpDocParser\Parser\PhpDocParser;
 use PHPStan\PhpDocParser\Parser\TokenIterator;
 use PHPStan\PhpDocParser\Parser\TypeParser;
+use PHPStan\PhpDocParser\ParserConfig;
+use ReflectionClass;
 use Valantic\PimcoreApiDocumentationBundle\Contract\Service\DocBlockParserInterface;
 use Valantic\PimcoreApiDocumentationBundle\Enum\TypeEnum;
 
 class DocBlockParser implements DocBlockParserInterface
 {
-    private readonly PhpParser $phpParser;
     private readonly Lexer $lexer;
     private readonly PhpDocParser $phpDocParser;
 
     public function __construct()
     {
-        $this->phpParser = new PhpParser();
-        $this->lexer = new Lexer();
+        $usedAttributes = [
+            'lines' => true,
+            'indexes' => true,
+            'comments' => true,
+        ];
 
-        $constExprParser = new ConstExprParser();
-        $typeParser = new TypeParser($constExprParser);
+        $config = new ParserConfig($usedAttributes);
 
-        $this->phpDocParser = new PhpDocParser($typeParser, $constExprParser);
+        $this->lexer = new Lexer($config);
+        $constExprParser = new ConstExprParser($config);
+        $typeParser = new TypeParser($config, $constExprParser);
+        $this->phpDocParser = new PhpDocParser($config, $typeParser, $constExprParser);
     }
 
     public function parseDocBlock(string $docBlock): array
@@ -40,6 +45,8 @@ class DocBlockParser implements DocBlockParserInterface
         $parsedDocBlock = $this->phpDocParser->parse($tokens);
 
         foreach ($parsedDocBlock->children as $docBlockItem) {
+            $parameterName = null;
+
             if ($docBlockItem->value instanceof ParamTagValueNode) {
                 $parameterName = ltrim($docBlockItem->value->parameterName, '$');
             }
@@ -48,7 +55,7 @@ class DocBlockParser implements DocBlockParserInterface
                 $parameterName = ltrim($docBlockItem->value->variableName, '$');
             }
 
-            if (isset($parameterName)) {
+            if ($parameterName) {
                 $docBlockData[$parameterName] = $docBlockItem;
             }
         }
@@ -56,30 +63,49 @@ class DocBlockParser implements DocBlockParserInterface
         return $docBlockData;
     }
 
-    public function determineTypeHint(PhpDocChildNode $docBlock, \ReflectionClass $reflectionClass): array
+    public function determineTypeHint(PhpDocChildNode $docBlock, ReflectionClass $reflectionClass): array
     {
-        $useStatements = $this->phpParser->parseUseStatements($reflectionClass);
-
+        $useStatements = $this->parseUseStatements($reflectionClass);
         $typeHint = $docBlock->value->type->type->name ?? null;
 
         if ($typeHint === null) {
             return [];
         }
 
-        if (array_key_exists(strtolower((string) $typeHint), $useStatements)) {
-            return [$useStatements[strtolower((string) $typeHint)]];
+        $typeHintStr = (string) $typeHint;
+
+        if (array_key_exists(strtolower($typeHintStr), $useStatements)) {
+            return [$useStatements[strtolower($typeHintStr)]];
         }
 
-        $classTypeHint = sprintf('%s\\%s', $reflectionClass->getNamespaceName(), $typeHint);
+        $classTypeHint = sprintf('%s\\%s', $reflectionClass->getNamespaceName(), $typeHintStr);
 
         if (class_exists($classTypeHint)) {
             return [$classTypeHint];
         }
 
-        if (TypeEnum::tryFrom($typeHint)) {
-            return [TypeEnum::tryFrom($typeHint)->swaggerEnum()];
+        if (TypeEnum::tryFrom($typeHintStr)) {
+            return [TypeEnum::tryFrom($typeHintStr)->swaggerEnum()];
         }
 
         return [];
+    }
+
+    private function parseUseStatements(ReflectionClass $class): array
+    {
+        $file = $class->getFileName();
+        $contents = file_get_contents($file);
+
+        preg_match_all('/use\s+([a-zA-Z0-9_\\\\]+)\s*;/', $contents, $matches);
+
+        $useStatements = [];
+
+        foreach ($matches[1] as $fqn) {
+            $parts = explode('\\', $fqn);
+            $alias = strtolower(end($parts));
+            $useStatements[$alias] = $fqn;
+        }
+
+        return $useStatements;
     }
 }
