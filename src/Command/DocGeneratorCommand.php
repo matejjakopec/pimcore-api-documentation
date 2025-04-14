58a09529bda4a7e4ce7381e39a9a224adcd76f24
mjakopec <matej.jakopec@factory.hr>
2025-04-14 02:15:16 +0200

    remove pimcore dependency

--- src/Command/DocGeneratorCommand.php
+++ src/Command/DocGeneratorCommand.php
@@ -4,34 +4,32 @@ declare(strict_types=1);
 
 namespace Valantic\PimcoreApiDocumentationBundle\Command;
 
-use Pimcore\Console\AbstractCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Valantic\PimcoreApiDocumentationBundle\Contract\Service\DocsGeneratorInterface;
 
-class DocGeneratorCommand extends AbstractCommand
+#[AsCommand(
+    name: 'valantic:api-doc:generate',
+    description: 'Generate API docs for controller actions.'
+)]
+class DocGeneratorCommand extends Command
 {
     public function __construct(
         private readonly DocsGeneratorInterface $docsGenerator,
         #[Autowire('%valantic.pimcore_api_doc.docs_file%')]
         private readonly string $filePath,
-        ?string $name = null,
     ) {
-        parent::__construct($name);
-    }
-
-    protected function configure(): void
-    {
-        $this
-            ->setName('valantic:api-doc:generate')
-            ->setDescription('Generate api docs for controller actions.');
+        parent::__construct();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->docsGenerator->generate($this->filePath);
+        $output->writeln('<info>API documentation generated successfully.</info>');
 
-        return self::SUCCESS;
+        return Command::SUCCESS;
     }
 }
